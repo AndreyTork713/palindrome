9d674a490bd06d92bf9d770a9bcb3eb7533de508
Andrey Kropotin <tork713@gmail.com>
2019-04-22 10:19:58 +0700

    рабочая версия Палиндрома

--- user_input/user_input.py
+++ user_input/user_input.py
@@ -5,11 +5,15 @@ def is_palindrome(text):
     return text == reverse(text)
 
 something = input('Введите текст: ')
+something = something.lower()
 
-forbidden =('.','?','!',':',';','-','—',' ')
+
+
+forbidden = ('.','?','!',':',';','-','—',' ')
+simbol = ' '
 if simbol in forbidden:
     if simbol in something:
-        something = something.replace(simbol,"")
+        something = something.replace(simbol,'')
    
 print(something)
 
@@ -17,3 +21,4 @@ if (is_palindrome(something)):
     print('Да это палиндром')
 else:
     print('НЕТ это не палиндром')
+
